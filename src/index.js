57b45712f4b185c7f71aeed608122177d6701f53
Phoebe Schmidt <phbschmidt@gmail.com>
2018-09-06 11:11:00 +0200

    Use apollo-client instead of apollo boost and new fragment scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import './index.css';
+import './public/stylesheets/style.css'
 import Home from './components/Home'
 import Courses from './components/Courses'
 
@@ -9,18 +9,41 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
-import ApolloClient from 'apollo-boost'
+import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
+import introspectionQueryResultData from './fragmentTypes.json';
 
-const SPACE_ID = '409ccxwy4hgv'
-const ACCESS_TOKEN = '821f63b9888bf6b2d48587d14be057bd51feab28f188b895cb0a87b22fa12f7b'
+const SPACE_ID = 'qz0n5cdakyl9'
+const ACCESS_TOKEN = '580d5944194846b690dd89b630a1cb98a0eef6a19b860ef71efc37ee8076ddb8'
 const localeCode = "en-US"
-
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData
+});
 const client = new ApolloClient({
-  uri: `https://cdn.contentful.com/spaces/${SPACE_ID}/graphql/alpha?locale=${localeCode}`,
-  headers: {
-    Authorization: `Bearer ${ACCESS_TOKEN}`
-  }
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: `https://cdn.contentful.com/spaces/${SPACE_ID}/graphql/alpha?locale=${localeCode}`,
+      credentials: 'same-origin',
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`
+      }
+    })
+  ]),
+  cache: new InMemoryCache({ fragmentMatcher })
 })
 
 
