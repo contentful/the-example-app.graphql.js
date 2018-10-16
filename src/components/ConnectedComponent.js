8ccac050634a355aaee55b8c11b0d58fbe7db6f3
Phoebe Schmidt <phbschmidt@gmail.com>
2018-10-16 11:54:40 +0200

    Refactored graphql-querying components to use ConnectedComponent

--- src/components/ConnectedComponent.js
+++ src/components/ConnectedComponent.js
@@ -3,13 +3,13 @@ import { Query } from 'react-apollo'
 import Loading from './Loading'
 import Error from './Error'
 
-const ConnectedComponent = ({query, success, errorCheck = () => false}) => {
-  return <Query query={query}>
+const ConnectedComponent = ({query, success, errorCheck = () => false, variables=null}) => {
+  return <Query query={query} variables={variables}>
   {
     ({loading, error, data}) => {
       if (loading) return <Loading />
       if (error || errorCheck(data)) return <Error />
-      success(data)
+      return success(data)
     }
   }
   </Query>
